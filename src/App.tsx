2d6a9289f3866f0c88dbfe7e29cdca44a072253a
Bryan Valeriano <bryan.b.valeriano@gmail.com>
2022-05-21 17:07:27 -0300

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-import { Button } from "./components/Button";
+//import { Button } from "./components/Button";
 //import { MovieCard } from "./components/MovieCard";
 
 import { SideBar } from "./components/SideBar";
@@ -9,7 +9,6 @@ import { Content } from "./components/Content";
 import { api } from "./services/api";
 
 import "./styles/global.scss";
-
 import "./styles/sidebar.scss";
 import "./styles/content.scss";
 
@@ -33,18 +32,10 @@ interface MovieProps {
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
 
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-
   const [movies, setMovies] = useState<MovieProps[]>([]);
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
     {} as GenreResponseProps
   );
-
-  useEffect(() => {
-    api.get<GenreResponseProps[]>("genres").then((response) => {
-      setGenres(response.data);
-    });
-  }, []);
 
   useEffect(() => {
     api
